ea00cb8a1f858dcabcf03aedd939f929ba8f1dbd
Rwtkkl <kyunglock278@gmail.com>
2024-10-01 15:14:58 +0900

    Add files via upload

---
+++ Hw1_Head.h
@@ -0,0 +1,40 @@
+#ifndef HW1_HEAD_H
+#define HW1_HEAD_H
+
+class Add {
+
+private:
+    int a, b;
+public:
+    void setValue(int x, int y);
+    int calculate();
+};
+
+class Sub {
+
+private:
+    int a, b;
+public:
+    void setValue(int x, int y);
+    int calculate();
+};
+
+class Mul {
+
+private:
+    int a, b;
+public:
+    void setValue(int x, int y);
+    int calculate();
+};
+
+class Div {
+private:
+    int a, b;
+public:
+    void setValue(int x, int y);
+    int calculate();
+};
+
+
+#endif
